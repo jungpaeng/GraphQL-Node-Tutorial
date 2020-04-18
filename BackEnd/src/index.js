97b8290e6cf769c3ad4131c1e887009e259ab990
jungpaeng <meis1541@naver.com>
2020-04-19 04:27:44 +0900

    [B] Add: Link type 및 Default Value, resolver 생성

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,16 +1,35 @@
 const {GraphQLServer} = require('graphql-yoga');
 
+let links = [{
+  id: "link-0",
+  url: "www.howtographql.com",
+  description: "Fullstack tutorial for GraphQL",
+}];
+
 const typeDefs = `
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+
   type Query {
     info: String!
+    feed: [Link!]!
   }
 `;
 
 const resolvers = {
+  Link: {
+    id: parent => parent.id,
+    description:  parent => parent.description,
+    url: parent => parent.url,
+  },
   Query: {
-    info: () => 'Hackernews Clone API',
+    info: () => "This is the API of a Hackernews Clone",
+    feed: () => links,
   }
-};
+}
 
 const server = new GraphQLServer({
   typeDefs,
