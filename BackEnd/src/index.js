ff8d7ec42376294574f4be6bdcd10e5a84f528fc
jungpaeng <meis1541@naver.com>
2020-04-19 05:48:59 +0900

    [B] Add: Prisma Resolver 연결

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,56 +1,26 @@
 const {GraphQLServer} = require('graphql-yoga');
-
-let links = [{
-  id: "link-0",
-  url: "www.howtographql.com",
-  description: "Fullstack tutorial for GraphQL",
-}];
-let idCount = links.length;
+const {prisma} = require('../generated/prisma-client');
 
 const resolvers = {
   Query: {
-    info: () => "This is the API of a Hackernews Clone",
-    link: (_, {id}) => links.filter(link => link.id === id)[0],
-    feed: () => links,
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: (root, args, context) => {
+      return context.prisma.links()
+    },
   },
   Mutation: {
-    post: (_, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-
-      return link;
-    },
-    updateLink: (_, {id, url, description}) => {
-      let updatedLink = null;
-      links = links.map(item => (
-        item.id === id ? updatedLink = {
-          id: item.id,
-          url: url || item.url,
-          description: description || item.description,
-        } : item
-      ));
-
-      return updatedLink;
-    },
-    deleteLink: (_, {id}) => {
-      let deletedLink = null;
-      links = links.filter(item => {
-        if (item.id === id) deletedLink = item;
-
-        return item.id !== id;
+        description: args.description,
       });
-
-      return deletedLink;
-    }
+    },
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
+  context: { prisma },
   resolvers,
 });
 
