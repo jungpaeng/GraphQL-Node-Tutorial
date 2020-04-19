3a1a57d3f54beca9a74ef02d59cc74e7effbb9d7
jungpaeng <meis1541@naver.com>
2020-04-19 15:42:01 +0900

    [B] Add: 추천 구독

--- BackEnd/src/resolvers/Subscription.js
+++ BackEnd/src/resolvers/Subscription.js
@@ -1,14 +1,26 @@
-const newLinkSubscripbe = (parent, args, context, info) => {
+const newLinkSubscribe = (parent, args, context, info) => {
   return context.prisma.$subscribe.link({
     mutation_in: ['CREATED']
   }).node();
 }
 
+const newVoteSubscribe = (parent, args, context, info) => {
+  return context.prisma.$subscribe.vote({
+    mutation_in: ['CREATED']
+  }).node();
+}
+
 const newLink = {
-  subscribe: newLinkSubscripbe,
+  subscribe: newLinkSubscribe,
+  resolve: payload => payload,
+};
+
+const newVote = {
+  subscribe: newVoteSubscribe,
   resolve: payload => payload,
 };
 
 module.exports = {
   newLink,
+  newVote,
 };
