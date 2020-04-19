08534b662bcf025bc449f9383c588eec9ba034b8
jungpaeng <meis1541@naver.com>
2020-04-20 02:43:47 +0900

    [F] Add: 로그인 및 회원가입 mutation 연결

--- FrontEnd/src/components/Login.js
+++ FrontEnd/src/components/Login.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import {useHistory} from 'react-router';
+import {useMutation} from '@apollo/react-hooks';
 import {AUTH_TOKEN} from '../constants';
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 const _saveUserData = token => {
   localStorage.setItem(AUTH_TOKEN, token)
 }
 
 const Login = () => {
+  const history = useHistory();
   const [{login, email, password, name}, setInfo] = React.useState({
     login: true,
     email: '',
     password: '',
     name: '',
+  });
+
+  const _confirm = async data => {
+    const {token} = login ? data.login : data.signup;
+    _saveUserData(token);
+    history.push('/');
+  };
+
+  const mutation = login ? LOGIN_MUTATION : SIGNUP_MUTATION;
+  const [submit] = useMutation(mutation, {
+    variables: {email, password, name},
+    onCompleted: data => _confirm(data),
   });
 
   return (
@@ -49,9 +81,7 @@ const Login = () => {
       <div className="flex mt3">
         <div
           className="pointer mr2 button"
-          onClick={async () => {
-
-          }}
+          onClick={submit}
         >
           {login ? "login" : "create account"}
         </div>
