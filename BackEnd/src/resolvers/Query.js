c1fa86a71d7153fb452fe07024ba1b835430e4c0
jungpaeng <meis1541@naver.com>
2020-04-20 00:44:51 +0900

    [B] Add: feed 필터링

--- BackEnd/src/resolvers/Query.js
+++ BackEnd/src/resolvers/Query.js
@@ -1,5 +1,12 @@
 const feed = (parent, args, context, info) => {
-  return context.prisma.links();
+  const where = args.filter ? {
+    OR: [
+      {description_contains: args.filter},
+      {url_contains: args.filter},
+    ],
+  } : {};
+
+  return context.prisma.links({where});
 };
 
 module.exports = {
